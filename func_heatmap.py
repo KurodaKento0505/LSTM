297e22ece3a0c248112ada049b08a12ccd7c62f4
KurodaKento0505 <kuroda.kento@image.iit.tsukuba.ac.jp>
2024-04-05 17:35:57 +0900

    train location images with LSTM

--- func_heatmap.py
+++ func_heatmap.py
@@ -2,6 +2,7 @@ import csv
 import pandas as pd
 import os
 import random
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
@@ -9,6 +10,12 @@ from PIL import Image
 
 
 def make_heatmap(df, data_length, grid_size_x, grid_size_y):
+
+    # RGB画像のシーケンスを作成する
+    rgb_image_sequence_np = np.zeros((data_length, grid_size_y, grid_size_x, 3), dtype=np.uint8)
+    
+    # gray画像のシーケンスを作成する
+    gray_image_sequence_np = np.zeros((data_length, grid_size_y, grid_size_x), dtype=np.float64)
 
     for i in range(data_length):
 
@@ -20,14 +27,8 @@ def make_heatmap(df, data_length, grid_size_x, grid_size_y):
         # RGB画像を作成する
         rgb_image_np = np.zeros((grid_size_y, grid_size_x, 3), dtype=np.uint8)
 
-        # RGB画像のシーケンスを作成する
-        rgb_image_sequence_np = np.zeros((data_length, grid_size_y, grid_size_x, 3), dtype=np.uint8)
-
         # gray画像を作成する
-        gray_image_np = np.zeros((grid_size_y, grid_size_x), dtype=np.uint8)
-
-        # gray画像のシーケンスを作成する
-        gray_image_sequence_np = np.zeros((data_length, grid_size_y, grid_size_x), dtype=np.uint8)
+        gray_image_np = np.zeros((grid_size_y, grid_size_x), dtype=np.float64)
 
         # ガウス分布のパラメータ
         mean = [0, 0]  # 平均
@@ -35,8 +36,8 @@ def make_heatmap(df, data_length, grid_size_x, grid_size_y):
 
         # 位置情報を格子状の行列に反映
         # ball
-        grid_ball_x = int(round(df.loc[i, 'start_x'] * 110)) + 1
-        grid_ball_y = int(round(df.loc[i, 'start_y'] * 70)) + 1
+        grid_ball_x = int(round(df.loc[i, 'start_x'] * 110 / 3)) + 1
+        grid_ball_y = int(round(df.loc[i, 'start_y'] * 70 / 3)) + 1
         # 格子状の行列に位置情報を反映 (人がいる場合は1)
         grid_ball_np[grid_ball_y, grid_ball_x] = 1.0
         grid_ball_np[grid_ball_y - 1, grid_ball_x] = 0.5
@@ -53,13 +54,13 @@ def make_heatmap(df, data_length, grid_size_x, grid_size_y):
             
             # teammate
             if df.loc[i, 'teammate_' + str(j) + '_x'] == -1.0:
-                pass
+                continue
             else:
-                grid_teammate_x = int(round(df.loc[i, 'teammate_' + str(j) + '_x'] * 110)) + 1
-                grid_teammate_y = int(round(df.loc[i, 'teammate_' + str(j) + '_y'] * 70)) + 1
+                grid_teammate_x = int(round(df.loc[i, 'teammate_' + str(j) + '_x'] * 110 / 3)) + 1
+                grid_teammate_y = int(round(df.loc[i, 'teammate_' + str(j) + '_y'] * 70 / 3)) + 1
 
-                if (grid_teammate_x >= 120) or (grid_teammate_y >= 80):
-                    pass
+                if (grid_teammate_x >= 120 / 3) or (grid_teammate_y >= 80 / 3):
+                    continue
 
                 else:
 
@@ -74,15 +75,17 @@ def make_heatmap(df, data_length, grid_size_x, grid_size_y):
                     grid_teammate_np[grid_teammate_y + 1, grid_teammate_x - 1] = 0.25
                     grid_teammate_np[grid_teammate_y + 1, grid_teammate_x + 1] = 0.25
 
+        for j in range(1,12):
+        
             # opponent
             if df.loc[i, 'opponent_' + str(j) + '_x'] == -1.0:
-                pass
+                continue
             else:
-                grid_opponent_x = int(round(df.loc[i, 'opponent_' + str(j) + '_x'] * 110)) + 1
-                grid_opponent_y = int(round(df.loc[i, 'opponent_' + str(j) + '_y'] * 70)) + 1
+                grid_opponent_x = int(round(df.loc[i, 'opponent_' + str(j) + '_x'] * 110 / 3)) + 1
+                grid_opponent_y = int(round(df.loc[i, 'opponent_' + str(j) + '_y'] * 70 / 3)) + 1
 
-                if (grid_opponent_x >= 120) or (grid_opponent_y >= 80):
-                    pass
+                if (grid_opponent_x >= 120 / 3) or (grid_opponent_y >= 80 / 3):
+                    continue
 
                 else:
 
@@ -97,7 +100,22 @@ def make_heatmap(df, data_length, grid_size_x, grid_size_y):
                     grid_opponent_np[grid_opponent_y + 1, grid_opponent_x - 1] = 0.25
                     grid_opponent_np[grid_opponent_y + 1, grid_opponent_x + 1] = 0.25
 
-        '''# 赤チャンネルにデータを代入
+        # グレースケール化
+        gray_image_np += grid_ball_np * 255.0 * 0.50 + grid_teammate_np * 255.0 * 0.25 + grid_opponent_np * 255.0 *  0.10
+
+        # gray画像のシーケンスを作成する
+        gray_image_sequence_np[i, :, :] = gray_image_np
+
+        # PILライブラリを使って画像を表示する
+        # image = Image.fromarray(rgb_image_np)
+        # image.save('rgb_image_' + str(i) + '.png')
+        # cv2.imwrite('gray_image_' + str(i) + '.png', gray_image_np)
+    
+    # return rgb_image_sequence_np
+    return gray_image_sequence_np
+
+
+'''# 赤チャンネルにデータを代入
         rgb_image_np[:, :, 0] = grid_ball_np * 255
 
         # 緑チャンネルにデータを代入
@@ -108,18 +126,3 @@ def make_heatmap(df, data_length, grid_size_x, grid_size_y):
 
         # RGB画像のシーケンスを作成する
         rgb_image_sequence_np[i] = rgb_image_np'''
-
-        # PILライブラリを使って画像を表示する
-        # image = Image.fromarray(rgb_image_np)
-        # image.show()
-
-        # グレースケール化
-        gray_image_np = grid_ball_np * 255 * 0.5 + grid_teammate_np * 255 * 0.3 + grid_opponent_np * 255 * 0.2
-
-        # gray画像のシーケンスを作成する
-        gray_image_sequence_np[i] = gray_image_np
-
-    # print(gray_image_sequence_np)
-
-    # return rgb_image_sequence_np
-    return gray_image_sequence_np
